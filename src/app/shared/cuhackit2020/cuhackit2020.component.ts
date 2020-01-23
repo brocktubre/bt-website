a19299c344d763dd8fd66c551dd9ff60362b7328
brocktubre <brocktubre@gmail.com>
2020-01-23 06:21:29 -0500

    Fixed build fail

--- src/app/shared/cuhackit2020/cuhackit2020.component.ts
+++ src/app/shared/cuhackit2020/cuhackit2020.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuhackit2020.component.css']
 })
 export class Cuhackit2020Component implements OnInit {
-
+  public year: number;
   constructor() { }
 
   ngOnInit() {
+    this.year = new Date().getFullYear();
   }
 
 }
