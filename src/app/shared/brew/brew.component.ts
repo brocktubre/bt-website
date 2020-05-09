768e1ece1a7c90463f7225a2da137b7137ce04d8
brocktubre <brocktubre@gmail.com>
2020-05-09 10:41:45 -0400

    Graphing working.

--- src/app/shared/brew/brew.component.ts
+++ src/app/shared/brew/brew.component.ts
@@ -1,15 +1,19 @@
 import { BrewService } from './brew.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as Chart from 'chart.js';
+import { BrewStatsObj } from '../models/brew-stats-object.model';
 
 @Component({
   selector: 'app-brew',
   templateUrl: './brew.component.html',
   styleUrls: ['./brew.component.css']
 })
-export class BrewComponent implements OnInit {
+export class BrewComponent implements OnInit, AfterViewInit {
   public year: number;
   public statsAvailable: boolean;
   public loadingStats: boolean;
+  public canvas: any;
+  public ctx: any;
 
   constructor(private brewService: BrewService) { }
 
@@ -21,12 +25,44 @@ export class BrewComponent implements OnInit {
       if (stats.length > 0) {
         console.log('There are brew stats in the Google sheet.');
         this.statsAvailable = true;
+        this.buildChart(stats);
       } else {
         console.log('No brew stats in the Google sheet.');
       }
       this.loadingStats = false;
     });
+
   }
+
+  ngAfterViewInit() {
+    // something
+  }
+
+  public buildChart(stats: Array<BrewStatsObj>) {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+          labels: stats.map(function(stat) {
+            return stat.date;
+          }),
+          datasets: [{
+              data: stats.map(function(stat) {
+                return stat.temperature;
+              }),
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {}
+    });
+  }
+
 
 
 }
